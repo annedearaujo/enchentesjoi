d6bc67911292974cd56385176cb82ec37bcf145d
Anne Araújo <108763060+annedearaujo@users.noreply.github.com>
2023-11-25 00:51:56 -0300

    versão inicial do app

--- app/src/main/java/com/example/appalertaenchentes/MainActivity.kt
+++ app/src/main/java/com/example/appalertaenchentes/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.appalertaenchentes
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -15,20 +17,61 @@ class MainActivity : AppCompatActivity() {
     // Método chamado quando a atividade é criada
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        Log.d("MainActivity", "onCreate MainActivity")
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            // Uso de apply para eliminar a necessidade de chamar binding várias vezes para realizar configurações adicionais no objeto.
+            setContentView(root)
+        }
 
         // Configuração do listener para o botão de relatar enchente
         binding.reportFloodButton.setOnClickListener {
             try {
-                // Tentativa de iniciar a atividade ReportFloodActivity
+                // Criar um Intent para a ReportFloodActivity
+                val intent = ReportFloodActivity.newIntent(this)
+
+                // Definir as flags para limpar a pilha de atividades e iniciar uma nova tarefa
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+
+                // Iniciar a atividade ReportFloodActivity
                 startActivity(Intent(this, ReportFloodActivity::class.java))
+            } catch (e: ActivityNotFoundException) {
+                // Tratamento de erro de não encontrada a atividade
+                Log.e("MainActivity", "Atividade não encontrada: ${e.message}")
+                Toast.makeText(this, "Atividade não encontrada", Toast.LENGTH_SHORT).show()
             } catch (e: Exception) {
-                // Em caso de erro, imprima o stack trace, faça log e exiba uma mensagem de erro
+                // Em caso de outro erro não tratado, imprima o stack trace, faça log e exiba uma mensagem de erro
                 e.printStackTrace()
                 Log.e("MainActivity", "Erro ao iniciar a ReportFloodActivity: ${e.message}")
                 Toast.makeText(this, "Erro ao iniciar a atividade", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        // Configuração do menu
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    // Se já estiver na tela inicial, não faz nada
+                    if (this is MainActivity) {
+                        return@setOnItemSelectedListener true
+                    }
+
+                    // Navegar para a tela inicial
+                    startActivity(newIntent(this))
+                    true
+                }
+                R.id.menu_reports -> {
+                    // Navegar para a tela de relatório
+                    startActivity(ReportFloodActivity.newIntent(this))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, ReportFloodActivity::class.java)
         }
     }
 }
--- app/src/main/java/com/example/appalertaenchentes/ReportFloodActivity.kt
+++ app/src/main/java/com/example/appalertaenchentes/ReportFloodActivity.kt
@@ -1,9 +1,12 @@
 package com.example.appalertaenchentes
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +18,11 @@ class ReportFloodActivity : AppCompatActivity() {
     // Declaração de variáveis
     private lateinit var binding: ActivityReportFloodBinding
     private lateinit var databaseHelper: DatabaseHelper
-    private val pickImageRequest = 1
+
+    // Declaração de gravityOptions como uma propriedade da classe
+    private val gravityOptions: Array<String> by lazy {
+        resources.getStringArray(R.array.gravity_options)
+    }
 
     // Inicialização do contrato para obter conteúdo da atividade
     private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -23,19 +30,88 @@ class ReportFloodActivity : AppCompatActivity() {
             // Obtenha a URI da imagem selecionada
             val imageUri = result.data?.data
 
-            // Configure a imagem no ImageView
-            binding.photosImageView.setImageURI(imageUri)
+            if (imageUri != null) {
+                // Configure a imagem no ImageView
+                binding.photosImageView.setImageURI(imageUri)
+            } else {
+                // Tratar o caso em que o usuário pressionou "Cancelar" na galeria
+                Toast.makeText(this, "Nenhuma imagem selecionada", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+    // Lista de opções de localização
+    private val locationOptions = arrayOf(
+        "Adhemar Garcia",
+        "America",
+        "Anita Garibaldi",
+        "Atiradores",
+        "Aventureiro",
+        "Boa Vista",
+        "Boehmerwald",
+        "Bom Retiro",
+        "Bucarein",
+        "Centro",
+        "Comasa",
+        "Costa e Silva",
+        "Espinheiros",
+        "Fatima",
+        "Floresta",
+        "Glória",
+        "Guanabara",
+        "Iririú",
+        "Itaum",
+        "Itinga",
+        "Parque Guarani",
+        "Jardim Iririú",
+        "Jardim Paraíso",
+        "Jardim Sophia",
+        "Jarivatuba",
+        "Jativoca",
+        "João Costa",
+        "Morro do Meio",
+        "Nova Brasília",
+        "Paranaguamirim",
+        "Petrópolis",
+        "Bom Retiro",
+        "Pirabeiraba",
+        "Profipo",
+        "Saguaçu",
+        "Santa Catarina",
+        "Santo Antônio",
+        "São Marcos",
+        "Ulysses Guimarães",
+        "Vila Cubatão",
+        "Vila Nova",
+        "Zona Industrial Norte",
+        "Zona Industrial Tupy"
+    )
 
     // Método chamado quando a atividade é criada
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("ReportFloodActivity", "onCreate ReportFloodActivity")
         binding = ActivityReportFloodBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configuração do botão de voltar
+        binding.backButton.setOnClickListener {
+            Log.d("ReportFloodActivity", "Botão de voltar pressionado")
+
+            // Encerrar a ReportFloodActivity
+            finish()
+        }
+
         // Inicialização do banco de dados
         databaseHelper = DatabaseHelper(this)
+
+        // Configuração das opções do Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, gravityOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.severitySpinner.adapter = adapter
+
+        // Definir a opção padrão no Spinner
+        binding.severitySpinner.setSelection(0)  // 0 representa a primeira posição no array, que é "Selecione uma gravidade"
 
         // Configuração de listeners para botões
         binding.reportFloodButton.setOnClickListener {
@@ -43,32 +119,23 @@ class ReportFloodActivity : AppCompatActivity() {
         }
 
         binding.selectImageButton.setOnClickListener {
-            // Adicione um Intent para abrir a galeria
+            Log.d("ReportFloodActivity", "Botão de seleção de imagem pressionado")
+            // Intent para abrir a galeria
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             getContent.launch(intent)
         }
-    }
 
-    // Método chamado quando o resultado de uma atividade é recebido
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == pickImageRequest && resultCode == Activity.RESULT_OK) {
-            // Obtenha a URI da imagem selecionada
-            val imageUri = data?.data
-
-            // Configure a imagem no ImageView
-            binding.photosImageView.setImageURI(imageUri)
-        }
+        // Configuração do AutoCompleteTextView
+        val locationAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, locationOptions)
+        binding.locationAutoComplete.setAdapter(locationAdapter)
     }
 
     // Método para relatar enchente
     private fun reportFlood() {
         // Obtenção de dados do formulário
-        val location = binding.locationEditText.text.toString()
+        val location = binding.locationAutoComplete.text.toString()
         val severity = binding.severitySpinner.selectedItem.toString()
         val description = binding.descriptionEditText.text.toString()
-        val photos = binding.photosImageView.drawable
 
         // Antes de validar os dados, limpe a mensagem de erro
         binding.severityInputLayout.error = null
@@ -77,18 +144,31 @@ class ReportFloodActivity : AppCompatActivity() {
         if (location.isEmpty()) {
             // Exibir mensagem de erro e definir erro no EditText
             Toast.makeText(this, "O campo localização é obrigatório.", Toast.LENGTH_SHORT).show()
-            binding.locationEditText.error = "O campo localização é obrigatório."
+            binding.locationAutoComplete.error = "O campo localização é obrigatório."
             return
         }
 
-        if (severity.isEmpty()) {
-            // Defina a mensagem de erro para o severityInputLayout
-            binding.severityInputLayout.error = "O campo gravidade é obrigatório."
+        if (severity == "Selecione a gravidade") {
+            // Mensagem de erro para o severityInputLayout
+            binding.severityInputLayout.error = "Selecione a gravidade válida."
             return
         }
 
         // Fornecer feedback ao usuário
+        showToast("Relatório enviado com sucesso!")
+
+        // Desabilitar o botão após enviar o relatório
         binding.reportFloodButton.text = "Relatório enviado"
         binding.reportFloodButton.isEnabled = false
+    }
+    // Método para exibir Toast
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, ReportFloodActivity::class.java)
+        }
     }
 }
